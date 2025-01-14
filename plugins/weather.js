2680c66898af7a56257262cde230985c8ee5bb38
frostbitecloudhost <157946678+frostbitecloudhost@users.noreply.github.com>
2025-01-15 03:56:16 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By JawadTechX*
+> *Stay Safe!*
 `;
         return reply(weather);
     } catch (e) {
