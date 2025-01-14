d65f8c0b668cd005688b2ff1c873219447f54bf9
frostbitecloudhost <157946678+frostbitecloudhost@users.noreply.github.com>
2025-01-15 03:36:23 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX
+♱ Bludos ♱
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
